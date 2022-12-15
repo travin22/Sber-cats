05e510b7daa29e3d38d0fe489d8fe32b8070e4cb
travin22 <travin-dima22@mail.ru>
2022-12-15 23:37:02 +0300

    Изменил стили, поправил код

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ const $wr = document.querySelector("[data-wr]");
 
 const getCatHTML = (cat) => {
   return `
-    <div class="card m-3" style="width: 18rem">
-          <img src="${cat.image}" class="card-img-top" alt="${cat.name}" />
-          <div class="card-body">
-            <h5 class="card-title">${cat.name}</h5>
-            <p class="card-text">
-            ${cat.description}
-            </p>
-            <a href="#" class="btn btn-primary">Подробнее</a>
-          </div>
-        </div>
-    `;
+  <div class="card">
+    <img src="${cat.image}" class="img" alt="${cat.name}" />
+    <div class="card-body">
+      <h2>${cat.name}</h2>
+      <p class="card_text">${cat.description}</p>
+      <a href="#" class="button">Подробнее</a>
+    </div>
+  </div>
+  `;
 };
 
 fetch("https://cats.petiteweb.dev/api/single/travin22/show/")
